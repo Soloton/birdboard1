36458fba94f1941a2a6c3b1d1ce159ac7f981efe
Soloton <arkhipov-ms@ya.ru>
2019-11-16 22:34:29 +0300

    Episode 31 complite

--- app/Project.php
+++ app/Project.php
@@ -74,6 +74,6 @@ class Project extends Model
      */
     public function members()
     {
-        return $this->belongsToMany(User::class, 'project_members');
+        return $this->belongsToMany(User::class, 'project_members')->withTimestamps();
     }
 }
