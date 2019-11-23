f67e6b015cfb1c14bcc97e869bc4c98b4187672c
Soloton <arkhipov-ms@ya.ru>
2019-11-23 12:09:24 +0300

    Episode 38
    Signed-off-by: Soloton <arkhipov-ms@ya.ru>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
-<body class="theme-dark bg-page">
+<body class="theme-light bg-page">
 <div id="app">
     <nav class="bg-header section">
         <div class="container mx-auto">
@@ -45,36 +45,31 @@
 
                 <div>
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto list-reset">
+                    <div class="flex items-center ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a
-                                    class="flex items-center text-default no-underline text-sm"
-                                    href="#" role="button"
-                                    data-toggle="dropdown"
-                                    aria-haspopup="true"
-                                    aria-expanded="false"
-                                    v-pre
-                                >
-                                    <img width="35"
-                                         class="rounded-full mr-3"
-                                         src="{{ gravatar_url(auth()->user()->email) }}">
+                            <theme-switcher></theme-switcher>
+                            <a
+                                class="flex items-center text-default no-underline text-sm"
+                                href="#" role="button"
+                                data-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false"
+                                v-pre
+                            >
+                                <img width="35"
+                                     class="rounded-full mr-3"
+                                     src="{{ gravatar_url(auth()->user()->email) }}">
 
-                                    JeffreyWay
-                                </a>
-                            </li>
+                                JeffreyWay
+                            </a>
                         @endguest
-                    </ul>
+                    </div>
                 </div>
             </div>
         </div>
